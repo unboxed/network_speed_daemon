3ddab6b3f222e3cddce96f9e078c91303987b143
Chris Holmes <tochrisholmes@gmail.com>
2016-12-05 10:29:44 +0000

    Initial commit

---
+++ lib/pickles/daemon.rb
@@ -0,0 +1,7 @@
+require "pickles/daemon/version"
+
+module Pickles
+  module Daemon
+    # Your code goes here...
+  end
+end
---
+++ lib/pickles/daemon/version.rb
@@ -0,0 +1,5 @@
+module Pickles
+  module Daemon
+    VERSION = "0.1.0"
+  end
+end
